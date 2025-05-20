97abe765f5eb2e4bd75a0b034a7f3a8894b9a5f0
Giselle <cryponiess@gmail.com>
2025-05-20 18:41:02 +1000

    removed unused declaration

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
-import { twMerge } from "tailwind-merge";
 import me from "../assets/me.png";
 
 const MainComponent = ({ show }: { show: boolean }) => {
-  const [render, setRender] = useState(false);
-
-  useEffect(() => {
-    if (show) {
-      setTimeout(() => {
-        setRender(true);
-        console.log("RENDER!");
-      }, 1000);
-    }
-  }, [show]);
-
   return (
     show && (
       <div className="zoom-element scene w-xl max-w-screen scene flex flex-col justify-center items-center">
-        <img
-          onLoad={() => setRender(true)}
-          src={me}
-          className="w-[60%] min-w-xs layer"
-        ></img>
+        <img src={me} className="w-[60%] min-w-xs layer"></img>
       </div>
     )
   );
